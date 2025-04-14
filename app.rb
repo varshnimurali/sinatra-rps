9b0f3ad1d9dda414a6e55f4921976e3b6b381dd8
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-14 17:18:16 +0000

    update

--- app.rb
+++ app.rb
@@ -2,58 +2,47 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
 end
 
 get("/rock") do
   moves = ["rock", "paper", "scissors"]
-  comp_move = moves.sample
-  if comp_move == "rock"
-    outcome = "tied"
-  elsif comp_move == "paper"
-    outcome = "lost"
-  else outcome = "won"
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else @outcome = "won"
   end
 
-  "
-  <h2>
-    We played rock!
-  </h2>
+  erb(:zebra)
 
-  <h2>
-   They played #{comp_move}!
-  </h2>
-
-  <h2>
-    We #{outcome}!
-  </h2>  
-  "
 end
 
 get("/paper") do
   moves = ["rock", "paper", "scissors"]
-  comp_move = moves.sample
-  if comp_move == "paper"
-    outcome = "tied"
-  elsif comp_move == "scissors"
-    outcome = "lost"
-  else outcome = "won"
+  @comp_move = moves.sample
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else @outcome = "won"
   end
 
-  "
-  <h2>
-    We played paper!
-  </h2>
+  erb(:paper)
 
-  <h2>
-   They played #{comp_move}!
-  </h2>
+end
 
-  <h2>
-    We #{outcome}!
-  </h2>  
-  "
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else @outcome = "won"
+  end
+
+  erb(:scissors)
+  
 end
