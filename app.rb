1c4874192f4e4da1e45367f4ba61c4175a885c51
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-14 16:41:45 +0000

    commit changes

--- app.rb
+++ app.rb
@@ -7,3 +7,25 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  comp_move = moves.sample
+  if comp_move == rock
+    outcome = "tied"
+    
+
+  "
+  <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+   They played #{comp_move}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>  
+  "
+end
